7743357bf7530b445c2d90515b0a8dbe708bbcae
Mac Lockard <maclockard@gmail.com>
2021-04-07 13:58:33 -0700

    Add monaco failure

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,6 +1,9 @@
 /// <reference types="cypress" />
 describe('page', () => {
   it('works', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit('https://microsoft.github.io/monaco-editor/')
+
+    cy.get('.monaco-editor').should('include.text', 'Game')
+    cy.get('.monaco-editor').should('include.text', 'Game of Life')
   })
 })
